00d0946fb2724e8bd579c867a0b4fe464122b79c
Alberto <alb@eag.com>
2020-05-22 19:47:27 +0200

    Corrección puerto

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ app.use('/', routes());
 
 // Servidor y puerto
 const host = process.env.HOST || '0.0.0.0';
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, host, () => {
     console.log('El servidor esta funcionando');
