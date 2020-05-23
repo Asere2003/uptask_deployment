37fa44a08a3d352795893d739ca89e0b91cb3e3a
Alberto <alb@eag.com>
2020-05-23 08:10:20 +0200

    correción

--- index.js
+++ index.js
@@ -76,7 +76,6 @@ app.use((req, res, next) => {
 app.use('/', routes());
 
 // Servidor y puerto
-const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 5000;
 
 app.listen(port, '0.0.0.0', () => {
